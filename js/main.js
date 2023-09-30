5330f5e9428149cf0a307bceaf3836e7299528d0
victoriaArteche <agostinavictoria4@gmail.com>
2023-09-30 01:20:36 -0300

    primeras pract dom

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let productoElegido = document.getElementsByClassName("productoElegido");
+let producto = document.getElementById("producto");
+
+console.log(productoElegido);
+console.log(producto.innerHTML);
